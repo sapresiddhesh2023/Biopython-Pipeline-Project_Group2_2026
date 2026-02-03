7fcb7d0fccd65147a0e607e523dd103cad2c86dc
Siddhesh Uday Sapre <sapresiddhesh2023@gmail.com>
2026-02-03 16:54:36 +0530

    Delete Step1_FASTA_Format.py

--- Step1_FASTA_Format.py
+++
@@ -1,8 +0,0 @@
-print("Step 1: performed by Siddhesh Uday Sapre")
-print("-"*90)
-from Bio import SeqIO 
-record = SeqIO.read("Otsutsugamushi_Karp_tsa47.fasta", "fasta")
-print("The following DNA record has Nucleotide ID:", record.id)
-print(record.description)
-print("The length of the sequence is:", len(record.seq), "bp")
-print("The DNA sequence is:", record.seq)
