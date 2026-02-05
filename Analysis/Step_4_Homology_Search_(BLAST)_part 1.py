ff2fba5cb7765bfd77e62e1d9d2244aa6c344a45
sameenkhaled-cmd <sameenkhaled@gmail.com>
2026-02-05 12:58:14 +0500

    Blast_results
    Updated the script to include email for Entrez and cleaned up imports. The protein sequence and BLAST search functionality remain intact.

---
+++ Analysis/Step_4_Homology_Search_(BLAST)_part 1.py
@@ -0,0 +1,37 @@
+from Bio.Blast import NCBIWWW
+from Bio import SeqIO
+from Bio.Blast import NCBIWWW
+from Bio.Blast import NCBIXML
+import ssl
+from Bio.SeqRecord import SeqRecord
+from Bio import Entrez
+Entrez.email = "sameenkhaled@gmail.com"
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Full protein sequence
+sequence = (
+    "MKKAFYLHLIVFALQGISNVHSKSLLNQKALLPQQKSDMHINVNSLSDIVEPLISTVVSIYAVDTNIGISFNNKV"
+    "SKYQQEVFLGSGVIIDSSGYIVTNENVIAGAENIKVKLHDGSELIAELVGSDNKINIALLKINSPAALSYATFGD"
+    "SNQSRVGDQVIAIGSPFGLRGTVTNGIISSKGRDMGNGIVTDFIQTNAAIHMGSFGGPMFNLEGKIIGINSIHVSY"
+    "SGISFAIPSNTVLEAVECLKKGEKIRRGMLNVMLNELTPELNENLGLKQDQNGVLITEVIKEGSAAQCGIAPGDV"
+    "ITKFHDKAIKTGRDLQVAVSSTMLNSEREVELLRNGKSMTLKCKIIANKGEDSEQQSNDQSLVVNGVKFVDLTPD"
+    "LVKKYNITSANNNGLFVLEVSPNSSWGRYGLKMGLRPRDIILSVKRDDNKKDISVKTLREIVTNIKHNEIFFTVQ"
+    "RGDRMLYIALPNINK"
+)
+
+print("Performing BLASTP search...")
+
+# Perform BLASTP
+result_handle = NCBIWWW.qblast(
+    program="blastp",
+    database="refseq_protein",
+    sequence=sequence,
+    hitlist_size=50
+)
+
+# Save results
+with open("blast_results.xml", "w") as out:
+    out.write(result_handle.read())
+
+print("BLAST completed and saved to blast_results.xml")
