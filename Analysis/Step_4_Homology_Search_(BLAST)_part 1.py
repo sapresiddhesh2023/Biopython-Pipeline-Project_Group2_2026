fcd077451753ee7d03ec64c8e359ade1e5211dd9
sameenkhaled-cmd <sameenkhaled@gmail.com>
2026-02-05 15:42:47 +0500

    Blast_results

--- Analysis/Step_4_Homology_Search_(BLAST)_part 1.py
+++ Analysis/Step_4_Homology_Search_(BLAST)_part 1.py
@@ -1,10 +1,5 @@
-from Bio import SeqIO
 from Bio.Blast import NCBIWWW
-from Bio.Blast import NCBIXML
 import ssl
-from Bio.SeqRecord import SeqRecord
-from Bio import Entrez
-Entrez.email = "sameenkhaled@gmail.com"
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -21,17 +16,16 @@ sequence = (
 
 print("Performing BLASTP search...")
 
-# Performing BLASTP
+# Perform BLASTP
 result_handle = NCBIWWW.qblast(
     program="blastp",
     database="refseq_protein",
     sequence=sequence,
     hitlist_size=50
 )
-
-# Saving results
 with open("blast_results.xml", "w") as b:
     b.write(result_handle.read())
 
 print("BLAST completed and saved to blast_results.xml")
+
 
